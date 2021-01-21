55b58dbf2b811a9add6cbf3a1b7634d3c3f032a6
Valeriy-Leonov <valeoprint@ukr.net>
2021-01-21 20:49:28 +0200

    Профиль социальной сети

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.scss';
+import Profile from "./components/Profile";
+import user from "./user.json";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />  
+      </div>
+  );
+}
+
+export default App;
