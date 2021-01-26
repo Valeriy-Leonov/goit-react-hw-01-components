c68312b98a24977bd20558a35337049870bc15f8
Valeriy-Leonov <valeoprint@ukr.net>
2021-01-26 20:18:08 +0200

    Привет в App

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import user from './user.json';
 
 function App() {
   return (
-    <div className="App">
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
+    <>
+      <h1>Привет </h1>
+      <div className="App">
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+    </>
   );
 }
 
